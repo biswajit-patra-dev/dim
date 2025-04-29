86da02273a525cec495c9bee92363457c1aa9ada
biswajit-patra-dev <biswajit.patra@debricked.com>
2025-04-29 16:31:56 +0530

    Modified pom file. Added vulnerable packages

--- src/main/java/rtes/App.java
+++ src/main/java/rtes/App.java
@@ -1,13 +1,21 @@
 package rtes;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class App {
+    private static final Logger logger = LogManager.getLogger(App.class);
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        processUserInput("Hello World!");
+    }
+
+    /**
+     * This function uses vulnerable dependencies in a way that could potentially
+     * expose the application to security risks.
+     */
+    public static void processUserInput(String userInput) {
+        logger.info("Processed user input: {}", userInput);
     }
 }
